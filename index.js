bcd854af7ac74e3c754fc8972ffdef289fb1f602
David <davidirvin47@gmail.com>
2020-12-12 18:05:00 -0700

    adds rotating carousel

--- index.js
+++ index.js
@@ -36,3 +36,86 @@ for (i=0; i<mobilePreviews.length; i++) {
         e.target.classList.toggle("mobile-preview-zoom");
     })
 }
+
+
+window.addEventListener("load", () => {
+    carousel(document.querySelector(".carousel"));
+})
+
+function carousel(root) {
+    var container = root.querySelector(".item-container"),
+        nav = root.querySelector("nav"),
+        headings = container.children,
+        n = headings.length,
+        html = document.querySelector("html"),
+        gap = parseFloat(getComputedStyle(html).width) > 640 ? 20 : 8,
+        theta = 2 * Math.PI / n,
+        currHead = 0
+    ;
+
+    setupCarousel(n, parseFloat(getComputedStyle(headings[0]).width));
+        window.addEventListener("resize", () => {
+            setupCarousel(n, parseFloat(getComputedStyle(headings[0]).width))
+        });
+        
+        setupNavigation();
+
+        function setupCarousel(n, s) {
+            var apothem = s / (2 * Math.tan(Math.PI / n));
+
+            container.style.transformOrigin = `50% 50% ${- apothem}px`;
+
+            for (i=0; i < n; i++) {
+                headings[i].style.padding = `0 ${gap}px`;
+                headings[i].style.backfaceVisibility = "hidden";
+            }
+            for (i=1; i < n; i++) {
+                headings[i].style.transformOrigin = `50% 50% ${- apothem}px`;
+                headings[i].style.transform = `rotateY(${i * theta}rad)`;
+            } 
+
+            rotateCarousel(currHead);
+        }
+
+        function setupNavigation() {
+            nav.addEventListener("click", onClick, true);
+
+            function onClick(e) {
+                e.stopPropagation();
+
+                var t = e.target;
+                if (t.tagName.toUpperCase() != 'BUTTON') {
+                    if (t.tagName.toUpperCase() != 'I')
+                    return;
+                }
+                 
+                if (t.classList.contains('next')) {
+                    removeZoom();
+                    currHead++;
+                }
+                if (t.classList.contains('fa-chevron-right')) {
+                    removeZoom();
+                    currHead++;
+                }
+                if (t.classList.contains('prev')) {
+                    removeZoom();
+                    currHead--;
+                }
+                if (t.classList.contains('fa-chevron-left')) {
+                    removeZoom();
+                    currHead--;
+                }
+
+                function removeZoom() {
+                    var zoomed = document.querySelector(".mobile-preview-zoom");
+                    zoomed && zoomed.classList.remove("mobile-preview-zoom");
+                }
+
+                rotateCarousel(currHead);
+            }
+        }
+
+        function rotateCarousel(headingIndex) {
+            container.style.transform = `rotateY(${headingIndex * - theta}rad)`;
+        }
+}
